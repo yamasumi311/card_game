0afbc035658e0f6cbb670d5aaccb0dcf91231a80
yamasumi311 <chim.0.don@gmail.com>
2023-07-12 21:37:40 +1000

    reformatted

--- cardGame.py
+++ cardGame.py
@@ -3,8 +3,8 @@ import time
 
 from functions import scoring, find_card_order, create_deck, play_one_round, end_game
 
-my_cards = [] # user's deck
-co_cards = [] # computer's deck
+my_cards = []  # user's deck
+co_cards = []  # computer's deck
 
 my_score = 0
 co_score = 0
@@ -17,14 +17,14 @@ print("In the end of game, the person who gets higher score wins!")
 print("Let's get started!")
 time.sleep(1)
 
-
 # 1 ask user to choose maisu and reveal the deck
 maisu = int(input('How many cards do you want to play?: '))
 my_cards = create_deck(maisu, my_cards)
 print(f"Your deck: {my_cards}")
 co_cards = create_deck(maisu, co_cards)
 
-def game(maisu, my_cards,co_cards,my_score,co_score):
+
+def game(maisu, my_cards, co_cards, my_score, co_score):
     for i in range(maisu):
         print("\n")
         round = i + 1
@@ -35,11 +35,13 @@ def game(maisu, my_cards,co_cards,my_score,co_score):
         my_choice = int(input('Choose the index number: '))
         # computer's choice
         co_choice = random.randint(0, maisu - 1 - i)
-        my_cards,co_cards,my_score,co_score = play_one_round(my_cards,co_cards,my_score,co_score, my_choice, co_choice)
+        my_cards, co_cards, my_score, co_score = play_one_round(my_cards, co_cards, my_score, co_score, my_choice,
+                                                                co_choice)
         i = i + 1
         # display the cards left
     return my_score, co_score
 
-my_score, co_score = game(maisu, my_cards,co_cards,my_score,co_score)
+
+my_score, co_score = game(maisu, my_cards, co_cards, my_score, co_score)
 
 end_game(my_score, co_score)
